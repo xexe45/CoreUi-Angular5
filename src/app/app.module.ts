4c32ff7283e0d478b3924acef85732df43d0e0f0
Cesar Lachira <lachiracesar@gmail.com>
2019-04-12 00:13:02 -0500

    Final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { APP_ROUTES } from './app.route';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { PagesModule } from './pages/pages.module';
-
+// Angular Material
+import { MatTabsModule } from '@angular/material/tabs';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,10 +17,12 @@ import { PagesModule } from './pages/pages.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     PagesModule,
     APP_ROUTES,
     ReactiveFormsModule,
     FormsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
